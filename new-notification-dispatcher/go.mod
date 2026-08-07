f58a7f6f223a70edc5d9422ba3e8952b24d4a505
DawidWit <135219147+DawidWit@users.noreply.github.com>
2026-08-07 22:35:59 +0200

    Using chi with Go

--- new-notification-dispatcher/go.mod
+++ new-notification-dispatcher/go.mod
@@ -1,3 +1,5 @@
-module github.com/DawidWit/Notification_Orchestrator/new-notification-dispatcher
+module github.com/DawidWit/Notification_Orchestrator
 
 go 1.26.5
+
+require github.com/go-chi/chi/v5 v5.0.8
--- new-notification-dispatcher/main.go
+++ new-notification-dispatcher/main.go
@@ -3,20 +3,20 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
 )
 
 func main() {
+	router := chi.NewRouter()
+
 	server := &http.Server{
 		Addr:    ":3000",
-		Handler: http.HandlerFunc(handler),
+		Handler: router,
 	}
 
 	err := server.ListenAndServe()
 	if err != nil {
-		fmt.Println("ERROR!")
+		fmt.Println("ERROR")
 	}
-}
-
-func handler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("New-notification-dispatcher"))
 }
